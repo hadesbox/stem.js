14298eae4d5c218b50a73536990fdb1a0c13529b
Luis Gonzalez <luis.gonzalez@beeva.com>
2013-04-12 09:51:08 +0200

    Renaming some.. stuff, you know.

--- templates/action_counter.js
+++ templates/action_counter.js
@@ -1,15 +1,11 @@
 function connection_listener(request, response) {
-
   var fs = require("fs");
-
   if (request.url == '/') {
       // Read the file.
     fs.readFile("<!CONF_FILENAME!>", 'utf-8', function (error, data) {
       // Write headers.
       response.writeHead(200, { 'Content-Type': 'text/plain' });
-
       console.log("read: ",data);
-
       if(isNaN(data)){
         data = 1;
       }
@@ -17,13 +13,11 @@ function connection_listener(request, response) {
         // Increment the number obtained from file.
         data = parseInt(data) + 1;
       }
-
       // Write incremented number to file.
       fs.writeFile('<!CONF_FILENAME!>', data);
       // End response with some nice message.
       response.end('This page was refreshed ' + data + ' times!');
     });
-
   } 
   else {
     // Indicate that requested file was not found.
@@ -31,5 +25,4 @@ function connection_listener(request, response) {
     // And end request without sending any data.
     response.end();
   }
-
 }
